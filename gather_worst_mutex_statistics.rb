eeb4a4b6bc5fd3df02c51209f2449c3b8c674a1d
Andres Jaan Tack <tack@cs.wisc.edu>
2008-10-08 02:08:28 +0000

    Added a script which processes the results of the BIND mutex statistics generator and orders them per their block-time/hold-time ratio.

---
+++ gather_worst_mutex_statistics.rb
@@ -0,0 +1,73 @@
+#
+# Parses the mutex trace data from BIND and produces from it the
+# N worst mutexes in the program. Badness is defined by a high
+# ratio of block time to hold-time.
+#
+NumberResultsReturned = ARGV[0].to_i
+
+if NumberResultsReturned.nil?
+	raise ArgumentError, "User should provide a number of results" +
+		" to return as the only parameter."
+end
+
+# Skip everything until "Mutex stats"
+while (aLine = $stdin.gets) 
+	if aLine.include?("Mutex stats")
+		break
+	end
+end
+
+# Each line is split into its five fields.
+class MutexStatLine
+	attr_reader :fileName, :lineNumber, :hits, :timeHeld, :timeBlocked
+	include Comparable
+	
+	def initialize(aLine)
+		piece = aLine.split(nil)
+		@fileName = piece[0]
+		@lineNumber = piece[1].to_i
+		@hits = piece[2].to_i
+		@timeHeld = piece[3].to_f
+		@timeBlocked = piece[4].to_f
+	end
+	
+	def blockRatio
+		if (timeHeld > 0)
+			timeBlocked / timeHeld
+		else
+			0
+		end
+	end
+	
+	def <=>(anotherMsl)
+		self.blockRatio <=> anotherMsl.blockRatio
+	end
+	
+	def to_s
+		"#{fileName}##{lineNumber}\t\t #{hits}\t#{timeHeld}\t#{timeBlocked}:\t#{blockRatio}"
+	end
+end
+
+# Gather individual lines from the file.
+line = Array.new
+while (aLine = $stdin.gets)
+	line.push(MutexStatLine.new(aLine))
+end
+
+def maximum (a, b)
+	if a > b then a else b end
+end
+# Print the N worst.
+line.delete_if do |theLine|
+	maximum(theLine.timeHeld, theLine.timeBlocked) < 0.001
+end
+
+line.sort!
+def minimum (a, b)
+	if a < b then a else b end
+end
+
+minimum(NumberResultsReturned, line.length).times do |time|
+	puts line.pop
+end
+
